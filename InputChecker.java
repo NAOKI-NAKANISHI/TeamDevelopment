d86a3f75e4ab18919135d362a81e2b65deafa3a5
“NAOKI-NAKANISHI” <xnbaxx@gmail.com>
2018-10-06 14:12:09 +0900

    Bitpattern method add

--- InputChecker.java
+++ InputChecker.java
@@ -18,6 +18,73 @@ public class InputChecker {
 	 * 例(質問の内容,値,最小文字数,最大文字数,true,false,false,true,true)となります。
 	 * 結果はList形式で渡されることに気を付けてください。
 	 ***/
+	public static final int TYPE_HIRAGANA = 1 << 0;
+	public static final int TYPE_KATAKANA = 1 << 1;
+	public static final int TYPE_KANJI    = 1 << 2;
+	public static final int TYPE_HANKAKU_NUMBER   = 1 << 3;
+	public static final int TYPE_HANKAKU_SYMBOL   = 1 << 4;
+	public static final int TYPE_HANKAKU_ALPHABET = 1 << 5;
+	
+	//ビットフィールドを用いた場合。ビットフィールドを用いるとdoCheckメソッドのように大量にフラグを渡す必要がなくなり、or演算子を用いて引数は1つで判定可能
+	//このテクニックは便利なので使えるようにする
+	public List<String> checkInputeString(String fieldName, String target, int type, int min, int max){
+		List<String> messageList = new ArrayList<String>();					//エラーメッセージを格納するためのリスト
+		if(type == 0) {
+			return messageList;
+		}
+        if (target.length() < min || target.length() > max) {
+        	messageList.add(fieldName + "は" + min + "文字以上" + max + "文字以下で入力してください。");
+        	return messageList;
+		}    
+		String[] patternList = {
+				"ぁ-ん",
+				"ァ-ヺ",
+				"一-龠",
+				"0-9",
+				"@.,;:!#$%&'*+-/=?^_`{|}~",
+				"a-zA-Z"
+		};
+		String[] allCharacterTypeList = {
+				"ひらがな",
+				"カタカナ",
+				"漢字",
+				"数字",
+				"記号",
+				"英字"
+		};
+		
+		String patternString = "^[";
+		List<String> requiredCharacterTypeList = new ArrayList<String>();
+		for(int patternIndex = 0;patternIndex <= 5;patternIndex++) {
+			if(((type >> patternIndex) & 1) == 0) {
+				continue;
+			}
+			String subsetString = patternList[patternIndex];
+			String description = allCharacterTypeList[patternIndex];
+			patternString += subsetString;
+			requiredCharacterTypeList.add(description);
+		}
+		patternString +="]+$";
+		
+		String requiredCharacterType = "";
+		for(int i = 0;i < requiredCharacterTypeList.size();i++){
+			if(i == 0){
+				requiredCharacterType += requiredCharacterTypeList.get(i).toString();
+			}else{
+				requiredCharacterType += "、" + requiredCharacterTypeList.get(i).toString();
+			}
+		}
+
+        Pattern patternCheck = Pattern.compile(patternString);//正規表現を用いてチェックを行う
+        Matcher matcher = patternCheck.matcher(target);
+        
+        if(!(matcher.find()) && messageList.isEmpty()) {
+        	messageList.add(fieldName + "は" + requiredCharacterType + "で入力してください。");
+	    }
+        
+		return messageList;
+	}
+	
 	public List<String> checkPattern(String type, String target, String triger, int min, int max) {
 		List<String> messageList = new ArrayList<String>();					//エラーメッセージを格納するためのリスト
 		HashMap<String, String> map = new HashMap<>();
@@ -31,6 +98,19 @@ public class InputChecker {
 		map.put("半角英数字", "\\b([0-9a-zA-Z])+\\b");
 		map.put("半角英数字、漢字、ひらがな、カタカナ、半角記号", "\\b([0-9a-zA-Z一-龠ぁ-ゞァ-ヶ-/:-@\\[-\\`\\{-\\~])+\\b");
 		map.put("半角数字", "\\b([0-9])+\\b");
+		map.put("半角英数字、漢字、ひらがな、カタカナ", "\\b([0-9a-zA-Z一-龠ぁ-ゞァ-ヶ])+\\b");
+		
+		/*	三項演算子を用いて強引に１行で書いた場合はこうなる。
+		 
+		 	String pattern = "半角英字、漢字、ひらがな".equals(triger) ? "\\b([一-龠a-zA-Zぁ-ゞ])+\\b" : 
+							 "ひらがな".equals(triger) ? "\\b([ぁ-ゞ])+\\b" : 
+							 "半角英数字、半角記号".equals(triger) ? "\\b([0-9a-zA-Z-/:-@\\[-\\`\\{-\\~])+\\b" : 
+							 "半角英数字".equals(triger) ? "\\b([0-9a-zA-Z])+\\b" :
+							 "半角英数字、漢字、ひらがな、カタカナ、半角記号".equals(triger) ? "\\b([0-9a-zA-Z一-龠ぁ-ゞァ-ヶ-/:-@\\[-\\`\\{-\\~])+\\b" : 
+							 "半角数字".equals(triger) ? "\\b([0-9])+\\b" : "\\b([0-9a-zA-Z一-龠ぁ-ゞァ-ヶ])+\\b";
+			
+			Pattern patternCheck = Pattern.compile(pattern));
+		*/
 		
         Pattern patternCheck = Pattern.compile(map.get(triger));//正規表現を用いてチェックを行う
         Matcher matcher = patternCheck.matcher(target);
