0245e7118073d8d35799a3808c7a2943117990e5
“NAOKI-NAKANISHI” <xnbaxx@gmail.com>
2018-10-23 10:41:05 +0900

    struts.xml session time out

---
+++ SessionInterceptor.java
@@ -0,0 +1,17 @@
+package com.internousdev.i1810a.util;
+
+import java.util.Map;
+
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
+
+public class SessionInterceptor extends AbstractInterceptor {
+	  @Override
+	  public String intercept(ActionInvocation invocation) throws Exception {
+	      Map<String,Object> session = invocation.getInvocationContext().getSession();
+	      if(session.isEmpty())
+	          return "session"; // session is empty/expired
+	      return invocation.invoke();
+	  }
+
+}
