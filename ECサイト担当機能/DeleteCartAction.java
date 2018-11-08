053b8b5dfcf728f578ec02d45073e0035a12034f
NAOKI-NAKANISHI <42198184+NAOKI-NAKANISHI@users.noreply.github.com>
2018-11-08 14:24:55 +0900

    Update DeleteCartAction.java
    使用していない変数を削除

--- ECサイト担当機能/DeleteCartAction.java
+++ ECサイト担当機能/DeleteCartAction.java
@@ -15,22 +15,7 @@ import com.opensymphony.xwork2.ActionSupport;
 
 public class DeleteCartAction extends ActionSupport implements SessionAware{
 	private Collection<String> checkList; //Cart.jspでチェックを入れた場合は値としてIDが入る
-	private String categoryId;
 	private String productId;
-	private String sex;
-	private List<String> sexList = new ArrayList<String>();
-	private static final String MALE = "男性";
-	private static final String FEMALE = "女性";
-	private String defaultSexValue = MALE;
-	private String productName;
-	private String productNameKana;
-	private String imageFilePath;
-	private String imageFileName;
-	private String price;
-	private String releaseCompany;
-	private String releaseDate;
-	private String productCount;
-	private String subtotal;
 	private String userId;
 	private List<MCategoryDTO> mCategoryDtoList = new ArrayList<MCategoryDTO>();
 
@@ -80,35 +65,11 @@ public class DeleteCartAction extends ActionSupport implements SessionAware{
 			int totalPrice = Integer.parseInt(String.valueOf(cartInfoDAO.getTotalPrice(userId)));
 			session.put("totalPrice", totalPrice);
 
-			sexList.add(MALE);
-			sexList.add(FEMALE);
+			sexList.add("男性");
+			sexList.add("女性");
 			result=SUCCESS;
 		}
 		return result;
-	}
-	public List<MCategoryDTO> getmCategoryDtoList() {
-		return mCategoryDtoList;
-	}
-	public void setmCategoryDtoList(List<MCategoryDTO> mCategoryDtoList) {
-		this.mCategoryDtoList = mCategoryDtoList;
-	}
-	public String getSex() {
-		return sex;
-	}
-	public void setSex(String sex) {
-		this.sex = sex;
-	}
-	public List<String> getSexList() {
-		return sexList;
-	}
-	public void setSexList(List<String> sexList) {
-		this.sexList = sexList;
-	}
-	public String getDefaultSexValue() {
-		return defaultSexValue;
-	}
-	public void setDefaultSexValue(String defaultSexValue) {
-		this.defaultSexValue = defaultSexValue;
 	}
 	public Collection<String> getCheckList() {
 		return checkList;
@@ -122,71 +83,11 @@ public class DeleteCartAction extends ActionSupport implements SessionAware{
 	public void setProductId(String productId) {
 		this.productId = productId;
 	}
-	public String getCategoryId() {
-		return categoryId;
-	}
-	public void setCategoryId(String categoryId) {
-		this.categoryId = categoryId;
-	}
 	public Map<String, Object> getSession() {
 		return session;
 	}
 	public void setSession(Map<String, Object> session) {
 		this.session = session;
-	}
-	public String getProductName() {
-		return productName;
-	}
-	public void setProductName(String productName) {
-		this.productName = productName;
-	}
-	public String getProductNameKana() {
-		return productNameKana;
-	}
-	public void setProductNameKana(String productNameKana) {
-		this.productNameKana = productNameKana;
-	}
-	public String getImageFilePath() {
-		return imageFilePath;
-	}
-	public void setImageFilePath(String imageFilePath) {
-		this.imageFilePath = imageFilePath;
-	}
-	public String getImageFileName() {
-		return imageFileName;
-	}
-	public void setImageFileName(String imageFileName) {
-		this.imageFileName = imageFileName;
-	}
-	public String getPrice() {
-		return price;
-	}
-	public void setPrice(String price) {
-		this.price = price;
-	}
-	public String getReleaseCompany() {
-		return releaseCompany;
-	}
-	public void setReleaseCompany(String releaseCompany) {
-		this.releaseCompany = releaseCompany;
-	}
-	public String getReleaseDate() {
-		return releaseDate;
-	}
-	public void setReleaseDate(String releaseDate) {
-		this.releaseDate = releaseDate;
-	}
-	public String getProductCount() {
-		return productCount;
-	}
-	public void setProductCount(String productCount) {
-		this.productCount = productCount;
-	}
-	public String getSubtotal() {
-		return subtotal;
-	}
-	public void setSubtotal(String subtotal) {
-		this.subtotal = subtotal;
 	}
 	public String getUserId() {
 		return userId;
